410eaff55c3846cdc8e205f28bf435f78f43c425
subrat <subratj611@gmail.com>
2023-03-23 02:21:07 +0530

    sidebar header chat update

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,6 +1,7 @@
 import "./sidebar.scss";
 
 import chatIcon from "../../assets/chat.png";
+import Chat from "../Chat";
 
 export default function SideBar() {
   return (
@@ -10,7 +11,32 @@ export default function SideBar() {
           <img src={chatIcon} alt="chat logo" className="chat-logo" />
           <h1 className="logo-title">ColdBurnChat</h1>
         </div>
-        <div>profile</div>
+        <div className="sidebar-profile">
+          <div className="sidebar-profile-img">
+            <img
+              src="https://api.dicebear.com/5.x/micah/svg?seed=Felix"
+              alt="profile-image"
+            />
+          </div>
+          <h2 className="sidebar-profile-name">flex frady</h2>
+          <div className="sidebar-profile-status">
+            <div className="status-active-icon"></div>
+            <span className="status-title">Active</span>
+          </div>
+        </div>
+      </div>
+      <div className="sidebar-chats">
+        <h2 className="sidebar-chats-title">Chats</h2>
+        <div className="sidebar-chats-user-container">
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+        </div>
       </div>
     </div>
   );
