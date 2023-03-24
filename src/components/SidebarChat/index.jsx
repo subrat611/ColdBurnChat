1c37fc45fba277059a01773c335f7514cec047d7
subrat <subratj611@gmail.com>
2023-03-25 00:59:40 +0530

    link add

--- src/components/SidebarChat/index.jsx
+++ src/components/SidebarChat/index.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 import "./chat.scss";
 
-export default function SidebarChat() {
+export default function SidebarChat({ roomId, chatName }) {
   return (
-    <div className="chat-wrapper">
-      <div className="chat-profile">
-        <img
-          src="https://api.dicebear.com/5.x/micah/svg?seed=Felix"
-          alt="avatar"
-        />
+    <Link to={`/rooms/${roomId}`}>
+      <div className="chat-wrapper">
+        <div className="chat-profile">
+          <img
+            src="https://api.dicebear.com/5.x/micah/svg?seed=Felix"
+            alt="avatar"
+          />
+        </div>
+        <div className="chat-profile-info">
+          <h3 className="chat-name">{chatName}</h3>
+          <p className="chat-last-message">last message...</p>
+        </div>
       </div>
-      <div className="chat-profile-info">
-        <h3 className="chat-name">Frady Lam</h3>
-        <p className="chat-last-message">last message...</p>
-      </div>
-    </div>
+    </Link>
   );
 }
