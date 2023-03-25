f45e6420e065d917375fa38702366074c7a2c8df
subrat <subratj611@gmail.com>
2023-03-25 18:17:31 +0530

    refactor : read data from firestore and then save to local state

--- src/components/context/User.jsx
+++ src/components/context/User.jsx
@@ -1,14 +1,42 @@
-import { createContext, useContext, useReducer } from "react";
-import { initialState, reducer } from "../reducer";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  onAuthStateChangedListener,
+  readCollectionFromFirestoreBasedOnId,
+  createUserInformationInFireStore,
+} from "../../firebase";
+import { initialState, reducer, SETUSER } from "../reducer";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  return (
-    <UserContext.Provider value={useReducer(reducer, initialState)}>
-      {children}
-    </UserContext.Provider>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = [state, dispatch];
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      setUserInfo(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  const setUserInfo = async (user) => {
+    if (user) {
+      const docSnap = await readCollectionFromFirestoreBasedOnId(
+        "rooms",
+        user.uid
+      );
+      if (docSnap.exists()) {
+        dispatch({
+          type: SETUSER,
+          user: user,
+          displayName: docSnap.data().name,
+          status: docSnap.data().status,
+        });
+      }
+    }
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
 
 export const useStateValue = () => useContext(UserContext);
